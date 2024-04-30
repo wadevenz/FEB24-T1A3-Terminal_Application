5884eeedf6afd644f86d3d50acf2beadb6aef7e0
[wadevenz] <[w.venz001@gmail.com]>
2024-04-30 19:36:08 +1000

    function created to return row in accordance with user pin input

--- src/bank_functions.py
+++ src/bank_functions.py
@@ -12,3 +12,12 @@ def create_account(main_file, name, pin, balance):
             writer = csv.writer(f)
             writer.writerow([name, pin, balance])
             print(f"\n{Fore.white}{Back.black}Thanks for opening an account with us {name}! As a bonus, ${balance} has been deposited into your account.{Style.reset}\n")
+        
+def get_account(main_file, pin):
+    with open(main_file, "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if row['pin'] == pin:
+                return row
+ 
+    
