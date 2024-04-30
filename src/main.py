f8d6e836fdc8f1c1b07752ea2ed10f0b67637781
[wadevenz] <[w.venz001@gmail.com]>
2024-04-30 18:02:27 +1000

    functions for requesting pin and name input

--- src/main.py
+++ src/main.py
@@ -7,3 +7,16 @@ print(f"\n{Fore.cyan}Welcome to Terminal Bank{Style.reset}\n")
 
 current_user = None
 initial_balance = 100
+
+def ask_name():
+    name = input("Please enter your First Name: ")
+    return name
+
+def ask_pin():
+    pin = input("Please enter a 4 digit PIN: ")
+    if not pin.isdigit():
+        print ("Please use only numbers!")
+    elif len(pin) != 4:
+        print("Valid PIN must be 4 digits!")
+    else:
+        return pin
