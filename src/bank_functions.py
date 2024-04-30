b83719c4256c2cf072f295cdcaa6bbab6fbab434
[wadevenz] <[w.venz001@gmail.com]>
2024-04-30 19:34:34 +1000

    function made for creating account and writing to file

--- src/bank_functions.py
+++ src/bank_functions.py
@@ -1,3 +1,14 @@
 import csv
 
 from colored import Fore, Back, Style
+
+
+def create_account(main_file, name, pin, balance):
+
+    if get_account(main_file, pin) != None:
+        print(f"\n{Fore.red}Same PIN already exists, please choose another.{Style.reset}\n")
+    else:
+        with open(main_file, "a") as f:
+            writer = csv.writer(f)
+            writer.writerow([name, pin, balance])
+            print(f"\n{Fore.white}{Back.black}Thanks for opening an account with us {name}! As a bonus, ${balance} has been deposited into your account.{Style.reset}\n")
