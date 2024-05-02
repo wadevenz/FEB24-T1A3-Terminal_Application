5de8a6dea4a6df8303fd08e82461bc2f23b1d68a
[wadevenz] <[w.venz001@gmail.com]>
2024-05-02 23:23:01 +1000

    adjustments to bash script

--- src/run.sh
+++ src/run.sh
@@ -2,15 +2,19 @@
 
 if ! [[ -x "$(command -v python)" ]]
 then
-  echo 'Error: Python is required to run this wpplication.
+  echo 'Error: Python is required to run this application.
     Please visit://installpython3.com/ to install.' >&2
   exit 1
 fi
 
-python3 -m venv .venv
-source .venv/bin/activate
-
-pip3 install colored
+if python3 -m venv .venv
+then
+  source .venv/bin/activate 
+  pip3 install -r src/requirements.txt
+  pip3 install colored
+else
+  echo Unable to create virtual environment
+fi
 
 python3 src/main.py
 
